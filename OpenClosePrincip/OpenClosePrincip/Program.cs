98daef69386cc5a2f5635d4c1143e7289af37a9a
JavidGasim <143398895+JavidGasim@users.noreply.github.com>
2023-12-03 16:51:40 +0400

    Add files via upload

---
+++ OpenClosePrincip/OpenClosePrincip/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace OpenClosePrincip;
+
+public abstract class Shape
+{
+    public abstract void Draw();
+}
+
+public class Circle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Circle is Drawing");
+    }
+}
+
+public class Rectangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Rectangle is Drawing.");
+    }
+}
+
+public class Drawer
+{
+    public void DrawShape(Shape shape)
+    {
+        shape.Draw();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Drawer drawer = new Drawer();
+
+        Shape circle = new Circle();
+        drawer.DrawShape(circle);
+
+        Shape rectangle = new Rectangle();
+        drawer.DrawShape(rectangle);
+    }
+}
